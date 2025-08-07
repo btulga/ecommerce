67ea35f2d9f32e19e6dca9fe845139051e9c7f52
btulga85 <btulga85@gmail.com>
2025-08-07 11:52:38 +0000

    medehgui ee noxoi

---
+++ src/models/address.model.js
@@ -0,0 +1,59 @@
+'use strict';
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Address extends Model {
+    static associate(models) {
+      // Address belongs to a Customer
+      Address.belongsTo(models.Customer, {
+        foreignKey: 'customer_id',
+        as: 'customer'
+      });
+
+      // An address can be a billing address for an Order
+      Address.hasMany(models.Order, {
+        foreignKey: 'billing_address_id',
+        as: 'billed_orders'
+      });
+
+      // An address can be a shipping address for an Order
+      Address.hasMany(models.Order, {
+        foreignKey: 'shipping_address_id',
+        as: 'shipped_orders'
+      });
+    }
+  }
+
+  Address.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    customer_id: {
+      type: DataTypes.UUID,
+      allowNull: true, // Allow addresses without a customer, e.g., guest checkouts
+      references: { model: 'customers', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    },
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    address_1: DataTypes.STRING,
+    address_2: DataTypes.STRING,
+    city: DataTypes.STRING,
+    country_code: DataTypes.STRING,
+    province: DataTypes.STRING,
+    postal_code: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    company: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Address',
+    tableName: 'addresses',
+    timestamps: true,
+  });
+
+  return Address;
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // Import existing routes
-const userRoutes = require('./userRoutes');
-const cartRoutes = require('./cartRoutes');
-const addressRoutes = require('./address.routes.js');
-const couponRoutes = require('./coupon.routes.js'); // Import new coupon routes
+const userRoutes = require('./user-routes');
+const cartRoutes = require('./cart-routes');
+const addressRoutes = require('./address-routes.js');
+const couponRoutes = require('./coupon-routes.js'); // Import new coupon routes
 
 // Register routes
 router.use('/users', userRoutes);
