5854dc4ff89ef5789abca3498d1aabac9f466d50
Tulga Battogtokh <btulga85@gmail.com>
2025-08-20 22:01:34 +0800

    feat: updated payment provider model

--- src/models/customer/payment-provider.model.js
+++ src/models/customer/payment-provider.model.js
@@ -1,26 +1,27 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class SalesChannelPaymentProvider extends Model {
+  class PaymentProvider extends Model {
     static associate(models) {
-      SalesChannelPaymentProvider.belongsTo(models.SalesChannel, {
+      PaymentProvider.belongsTo(models.SalesChannel, {
         foreignKey: 'sales_channel_id',
         as: 'sales_channel',
       })
     }
   }
-  SalesChannelPaymentProvider.init({
+  PaymentProvider.init({
     id: { type: DataTypes.STRING, defaultValue: DataTypes.UUIDV4, primaryKey: true },
     sales_channel_id: { type: DataTypes.STRING, allowNull: false },
     title: { type: DataTypes.TEXT, allowNull: false },
-    payment_provider_id: { type: DataTypes.STRING, allowNull: false },
+    provider_id: { type: DataTypes.STRING, allowNull: false }, //qpay, socialpay, etc
+    is_active: { type: DataTypes.BOOLEAN, defaultValue: true },
   }, {
     sequelize,
-    modelName: 'SalesChannelPaymentProvider',
-    tableName: 'sales_channel_payment_provider',
+    modelName: 'PaymentProvider',
+    tableName: 'payment_provider',
     timestamps: true,
     underscored: true,
-    indexes: [{ unique: true, fields: ['sales_channel_id', 'payment_provider_id'] }]
+    indexes: [{ unique: true, fields: ['sales_channel_id', 'provider_id'] }]
   });
-  return SalesChannelPaymentProvider;
+  return PaymentProvider;
 };
